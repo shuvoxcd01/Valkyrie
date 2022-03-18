bb2bdbce4d455ba4193de2659d2ae853aab5cfcc
Falguni Das <dasshuvo7@gmail.com>
2022-03-18 06:58:09 +0000

    Refactor and add type hint

--- src/driver/driver_factory.py
+++ src/driver/driver_factory.py
@@ -1,8 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Union
+from tf_agents.environments import PyEnvironment, TFEnvironment
 
 
 class DriverFactory(ABC):
+    def __init__(self, env: Union[PyEnvironment, TFEnvironment], observers: List) -> None:
+        self.env = env
+        self.observers = observers
+
     @abstractmethod
     def get_driver(self, policy, num_steps):
         pass
--- src/driver/dynamic_step_driver_factory.py
+++ src/driver/dynamic_step_driver_factory.py
@@ -1,15 +1,15 @@
 from typing import List
 from driver.driver_factory import DriverFactory
 from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import TFEnvironment
+from tf_agents.policies import tf_policy
 
 
 class DynamicStepDriverFactory(DriverFactory):
-    def __init__(self, env, observers:List) -> None:
-        super().__init__()
-        self.env = env
-        self.observers = observers
+    def __init__(self, env: TFEnvironment, observers: List) -> None:
+        super().__init__(env=env, observers=observers)
 
-    def get_driver(self, policy, num_steps):
+    def get_driver(self, policy: tf_policy.TFPolicy, num_steps: int):
         return dynamic_step_driver.DynamicStepDriver(
             env=self.env,
             policy=policy,
