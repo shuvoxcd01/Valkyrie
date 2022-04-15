d549ec3988df709626d8b4cade700f280d22cc59
Falguni Das <dasshuvo7@gmail.com>
2022-04-15 15:17:39 +0000

    Add support for population based training

--- src/main.py
+++ src/main.py
@@ -1,37 +1,35 @@
-from pickletools import optimize
-from agent.ddqn_agent_factory import DdqnAgentFactory
-from environment.cartpole_factory import CartPoleFactory
-from driver.py_driver_factory import PyDriverFactory
-from replay_buffer.reverb_replay_buffer_manager import ReverbReplayBufferManager
-from agent.dqn_agent_factory import DqnAgentFactory
-from driver.dynamic_step_driver_factory import DynamicStepDriverFactory
-from environment.pong_factory import PongFactory
-from network.atari_q_network_factory import AtariQNetworkFactory
-from replay_buffer.replay_buffer_manager import ReplayBufferManager
-from training.training import Training
-import tensorflow as tf
-from tf_agents.policies import random_tf_policy, random_py_policy, py_tf_eager_policy
-from tf_agents.utils import common
 import os
+import tensorflow as tf
+from training.population_based_training.population_based_training import PopulationBasedTraining
+from driver.py_driver_factory import PyDriverFactory
+from fitness_evaluator.fitness_evaluator import FitnessEvaluator
+from checkpoint_manager.replay_buffer_checkpoint_manager import ReplayBufferCheckpointManager
+from replay_buffer.reverb_replay_buffer_manager import ReverbReplayBufferManager
+from training.gradient_based_training.gradient_based_training import GradientBasedTraining
+from agent.meta_agent.meta_agent import MetaAgent
+from checkpoint_manager.agent_checkpoint_manager import AgentCheckpointManager
+from network.atari_q_network_factory import AtariQNetworkFactory
+from environment.cartpole_factory import CartPoleFactory
+from agent.tf_agent.ddqn_agent_factory import DdqnAgentFactory
 from tf_agents.environments import suite_atari, suite_gym, tf_py_environment, batched_py_environment, parallel_py_environment
 
 
-NUM_ITERATIONS = 250000
-
-INITIAL_COLLECT_STEPS = 100
-COLLECT_STEPS_PER_ITERATION = 1
+FC_LAYER_PARAMS = (512,)
+CONV_LAYER_PARAMS = None  # ((32, (8, 8), 4), (64, (4, 4), 2), (64, (3, 3), 1))
+INITIAL_LEARNING_RATE = 0.001
+TARGET_UPDATE_PERIOD = 50
 REPLAY_BUFFER_MAX_LENGTH = 100000
 
 BATCH_SIZE = 64
-LEARNING_RATE = 0.001
 LOG_INTERVAL = 200
-
 NUM_EVAL_EPISODES = 10
 EVAL_INTERVAL = 1000
-TARGET_UPDATE_PERIOD = 50
 
-FC_LAYER_PARAMS = (512,)
-CONV_LAYER_PARAMS = None  # ((32, (8, 8), 4), (64, (4, 4), 2), (64, (3, 3), 1))
+POPSIZE = 5
+NUM_GRADIENT_BASED_TRAINING_EPOCH = 10000
+CHECKPOINT_BASE_DIR = os.path.join(os.path.dirname(__file__), "checkpoints")
+
+initial_population = []
 
 env_factory = CartPoleFactory()
 
@@ -40,7 +38,6 @@ train_env = tf_py_environment.TFPyEnvironment(train_py_env)
 eval_env = env_factory.get_tf_env()
 
 train_env_observation_spec = train_env.observation_spec()
-# assert train_env.observation_spec() == eval_env.observation_spec()
 action_spec = train_env.action_spec()
 time_step_spec = train_env.time_step_spec()
 
@@ -48,60 +45,54 @@ time_step_spec = train_env.time_step_spec()
 network_factory = AtariQNetworkFactory(input_tensor_spec=train_env_observation_spec,
                                        action_spec=action_spec, conv_layer_params=CONV_LAYER_PARAMS,
                                        fc_layer_params=FC_LAYER_PARAMS)
-network = network_factory.get_network()
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+agent_factory = DdqnAgentFactory(time_step_spec=time_step_spec,
+                                 action_spec=action_spec, target_update_period=TARGET_UPDATE_PERIOD)
 
-agent_factory = DdqnAgentFactory(
-    time_step_spec=time_step_spec, action_spec=action_spec,
-    q_network=network, optimizer=optimizer, target_update_period=TARGET_UPDATE_PERIOD,
-    train_step_counter=tf.Variable(0))
+for i in range(POPSIZE):
+    network = network_factory.get_network()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=INITIAL_LEARNING_RATE)
+    train_step_counter = tf.Variable(0)
+    tf_agent = agent_factory.get_agent(name="agent_"+str(i), network=network,
+                                       optimizer=optimizer,
+                                       train_step_counter=train_step_counter)
 
-agent = agent_factory.get_agent()
-# agent_policy = py_tf_eager_policy.PyTFEagerPolicy(
-#     agent.policy, use_tf_function=True)
-# agent_collect_policy = py_tf_eager_policy.PyTFEagerPolicy(
-#     agent.collect_policy, use_tf_function=True)
+    meta_agent = MetaAgent(tf_agent=tf_agent)
+    initial_population.append(meta_agent)
 
-agent_py_policy = py_tf_eager_policy.PyTFEagerPolicy(
-    agent.policy, use_tf_function=True)
-agent_py_collect_policy = py_tf_eager_policy.PyTFEagerPolicy(
-    agent.collect_policy, use_tf_function=True)
+agent_checkpoint_manager = AgentCheckpointManager(
+    base_ckpt_dir=CHECKPOINT_BASE_DIR)
 
-print(agent.name)
+collect_data_spec = tf_agent.collect_data_spec
 
 replay_buffer_manager = ReverbReplayBufferManager(
-    data_spec=agent.collect_data_spec, replay_buffer_capacity=REPLAY_BUFFER_MAX_LENGTH)
-replay_buffer_observer = replay_buffer_manager.get_observer()
+    data_spec=collect_data_spec, replay_buffer_capacity=REPLAY_BUFFER_MAX_LENGTH)
 
-driver_factory = PyDriverFactory(
+
+replay_buffer_checkpoint_manager = ReplayBufferCheckpointManager(
+    base_ckpt_dir=CHECKPOINT_BASE_DIR)
+replay_buffer_checkpointer = replay_buffer_checkpoint_manager.create_or_initialize_checkpointer(
+    replay_buffer_manager.get_replay_buffer())
+
+fitness_evaluator = FitnessEvaluator(
+    environment=eval_env, num_episodes=NUM_EVAL_EPISODES)
+
+gradient_based_trainer = GradientBasedTraining(
+    train_env=train_py_env, replay_buffer_manager=replay_buffer_manager,
+    replay_buffer_checkpointer=replay_buffer_checkpointer,
+    fitness_evaluator=fitness_evaluator, num_train_iteration=NUM_GRADIENT_BASED_TRAINING_EPOCH,
+    log_interval=LOG_INTERVAL, eval_interval=EVAL_INTERVAL, batch_size=BATCH_SIZE)
+
+replay_buffer_observer = replay_buffer_manager.get_observer()
+collect_driver_factory = PyDriverFactory(
     env=train_py_env, observers=[replay_buffer_observer])
 
-random_policy = random_py_policy.RandomPyPolicy(
-    time_step_spec=time_step_spec,
-    action_spec=action_spec
-)
 
-initial_collect_driver = driver_factory.get_driver(
-    policy=random_policy, num_steps=INITIAL_COLLECT_STEPS)
+population_based_training = PopulationBasedTraining(initial_population=initial_population,
+                                                    agent_ckpt_manager=agent_checkpoint_manager,
+                                                    gradient_based_trainer=gradient_based_trainer,
+                                                    agent_factory=agent_factory,
+                                                    fitness_evaluator=fitness_evaluator,
+                                                    collect_driver_factory=collect_driver_factory)
 
-collect_driver = driver_factory.get_driver(
-    policy=agent_py_collect_policy, num_steps=COLLECT_STEPS_PER_ITERATION)
-
-# checkpoint_dir = os.path.join(os.path.dirname(__file__), "checkpoint")
-
-# checkpointer = common.Checkpointer(
-#     ckpt_dir=checkpoint_dir,
-#     max_to_keep=1,
-#     agent=agent,
-#     policy=agent.policy,
-#     replay_buffer=replay_buffer_manager.replay_buffer
-# )
-
-training = Training(agent=agent, collect_driver=collect_driver, train_env=train_py_env, eval_env=eval_env,
-                    replay_buffer_manager=replay_buffer_manager, logdir="./logs", train_checkpointer=None)
-
-initial_collect_driver.run(train_py_env.reset())
-
-training.train_agent(num_iterations=NUM_ITERATIONS, num_eval_episodes=NUM_EVAL_EPISODES,
-                     log_interval=LOG_INTERVAL, eval_interval=EVAL_INTERVAL, batch_size=BATCH_SIZE)
+population_based_training.train()
