0d8cee5e441c80a31677a34cb5758885ce4e5e82
Falguni Das <dasshuvo7@gmail.com>
2022-04-16 19:28:12 +0000

    Refactor

--- src/training/gradient_based_training/gradient_based_training.py
+++ src/training/gradient_based_training/gradient_based_training.py
@@ -4,11 +4,12 @@ from tf_agents.agents import TFAgent
 from tf_agents.utils import common
 from tf_agents.drivers.driver import Driver
 from tf_agents.environments import TFEnvironment, PyEnvironment
-from typing import Union
+from typing import Optional, Union
 import tensorflow as tf
 from tf_agents.environments import TFEnvironment, PyEnvironment
 from tf_agents.policies import py_policy, tf_policy, py_tf_eager_policy
-from agent.meta_agent.meta_agent import MetaAgent
+from driver.driver_factory import DriverFactory
+from agent.meta_agent.meta_q_agent.meta_q_agent import MetaQAgent
 from checkpoint_manager.replay_buffer_checkpoint_manager import ReplayBufferCheckpointManager
 from fitness_evaluator.fitness_evaluator import FitnessEvaluator
 from replay_buffer.replay_buffer_manager import ReplayBufferManager
@@ -24,7 +25,11 @@ class GradientBasedTraining:
                  initial_collect_driver: Driver,
                  fitness_evaluator: FitnessEvaluator,
                  num_train_iteration: int, log_interval: int, eval_interval: int,
-                 batch_size: int = 32) -> None:
+                 collect_driver_factory: DriverFactory,
+                 max_collect_steps: int,
+                 max_collect_episodes: int = 1,
+                 batch_size: int = 32,
+                 best_possible_fitness: Optional[int] = None) -> None:
 
         self.train_env = train_env
         self.replay_buffer_manager = replay_buffer_manager
@@ -36,23 +41,30 @@ class GradientBasedTraining:
         self.log_interval = log_interval
         self.batch_size = batch_size
         self.initial_collect_driver = initial_collect_driver
+        self.best_possible_fitness = best_possible_fitness
+        self.collect_driver_factor = collect_driver_factory
+        self.max_collect_steps = max_collect_steps
+        self.max_collect_episodes = max_collect_episodes
 
         self._initialize()
 
     def _initialize(self):
         self.initial_collect_driver.run(self.train_env.reset())
 
-    def _compute_avg_return(self, policy):
-        avg_return = self.fitness_evaluator.evaluate_fitness(policy)
+    def _get_collect_driver(self, tf_agent: TFAgent):
+        collect_policy = py_tf_eager_policy.PyTFEagerPolicy(
+            tf_agent.collect_policy, use_tf_function=True)
 
-        return avg_return
+        collect_driver = self.collect_driver_factor.get_driver(
+            policy=collect_policy, max_steps=self.max_collect_steps, max_episodes=self.max_collect_episodes)
 
-    def train_agent(self, meta_agent: MetaAgent, collect_driver: Driver):
+        return collect_driver
+
+    def train_agent(self, meta_agent: MetaQAgent):
         tf_agent = meta_agent.tf_agent
-        tf_agent.train = common.function(tf_agent.train)
+        collect_driver = self._get_collect_driver(tf_agent=tf_agent)
 
-        avg_return = self._compute_avg_return(tf_agent.policy)
-        returns = [avg_return]
+        tf_agent.train = common.function(tf_agent.train)
 
         iterator = self.replay_buffer_manager.get_dataset_iterator(
             num_parallel_calls=3, batch_size=self.batch_size, num_steps=2, num_prefetch=3)
@@ -70,15 +82,20 @@ class GradientBasedTraining:
                     'step = {0}: loss = {1}'.format(step, train_loss))
 
             if step % self.eval_interval == 0:
-                avg_return = self._compute_avg_return(tf_agent.policy)
+                fitness = self.fitness_evaluator.evaluate_fitness(
+                    tf_agent.policy)
+                meta_agent.update_fitness(fitness)
+
                 self.logger.info('step = {0}: Average Return = {1}'.format(
-                    step, avg_return))
+                    step, fitness))
 
                 meta_agent.summary_writer_manager.write_scalar_summary(
-                    name="Average return", data=avg_return)
-                returns.append(avg_return)
+                    name="Average return", data=fitness)
 
                 meta_agent.checkpoint_manager.save_checkpointer()
+
+                if self.best_possible_fitness and meta_agent.fitness >= self.best_possible_fitness:
+                    break
 
         meta_agent.checkpoint_manager.save_checkpointer()
 
--- src/training/population_based_training/population_based_training.py
+++ src/training/population_based_training/population_based_training.py
@@ -1,13 +1,13 @@
-from curses import meta
 import imp
 import tensorflow as tf
-from pickletools import optimize
-from typing import List, Union
+from typing import List, Optional, Union
 from tf_agents.agents import TFAgent
+from agent.meta_agent.meta_agent import MetaAgent
+from agent.meta_agent.meta_agent_copier import MetaAgentCopier
 from summary_writer.summay_writer_manager_factory import SummaryWriterManagerFactory
 from checkpoint_manager.agent_checkpoint_manager_factory import AgentCheckpointManagerFactory
 from driver.driver_factory import DriverFactory
-from agent.meta_agent.meta_agent import MetaAgent
+from agent.meta_agent.meta_q_agent.meta_q_agent import MetaQAgent
 from tf_agents.environments import TFEnvironment, PyEnvironment
 from agent.tf_agent.agent_factory import AgentFactory
 
@@ -22,11 +22,9 @@ from training.gradient_based_training.gradient_based_training import GradientBas
 class PopulationBasedTraining:
     def __init__(self, initial_population: List[MetaAgent],
                  gradient_based_trainer: GradientBasedTraining,
-                 agent_factory: AgentFactory,
                  fitness_evaluator: FitnessEvaluator,
-                 collect_driver_factory: DriverFactory,
-                 agent_ckpt_manager_factory: AgentCheckpointManagerFactory,
-                 summary_writer_manager_factory: SummaryWriterManagerFactory
+                 agent_copier: MetaAgentCopier,
+                 best_possible_fitness: Optional[int] = None
                  ) -> None:
 
         self.population = initial_population
@@ -34,12 +32,10 @@ class PopulationBasedTraining:
         self.best = None
 
         self.gradient_based_trainer = gradient_based_trainer
-        self.agent_factory = agent_factory
         self.fitness_evaluator = fitness_evaluator
-        self.collect_driver_factory = collect_driver_factory
+        self.agent_copier = agent_copier
 
-        self.agent_ckpt_manager_factor = agent_ckpt_manager_factory
-        self.summary_writer_manager_factory = summary_writer_manager_factory
+        self.best_possible_fitness = best_possible_fitness
 
     def assess_fitness(self, meta_agent: MetaAgent) -> float:
         policy = meta_agent.tf_agent.policy
@@ -51,43 +47,21 @@ class PopulationBasedTraining:
         for meta_agent in self.population:
             meta_agent.previous_fitness = meta_agent.fitness
 
-            self.train_with_gradient_based_approach(meta_agent)
+            self.gradient_based_trainer.train_agent(meta_agent=meta_agent)
 
             meta_agent.fitness = self.assess_fitness(meta_agent=meta_agent)
 
             if self.best is None or (meta_agent.fitness >= self.best.fitness):
-                # ToDo: Do proper cloning
-                q_net = meta_agent.tf_agent._q_network.copy()
-                q_net.create_variables()
-                q_net.set_weights(meta_agent.tf_agent._q_network.get_weights())
-                learning_rate = meta_agent.tf_agent._optimizer.learning_rate.numpy()
-                optimizer = tf.keras.optimizers.Adam(
-                    learning_rate=learning_rate)
-                training_step_counter = tf.Variable(
-                    meta_agent.tf_agent.train_step_counter.numpy())
-                best_tf_agent = self.agent_factory.get_agent(
-                    name="best", network=q_net, optimizer=optimizer, train_step_counter=training_step_counter)
+                if self.best:
+                    self.best.checkpoint_manager.delete_checkpointer()
 
-                agent_checkpoint_manager = self.agent_ckpt_manager_factor.get_agent_checkpoint_manager(
-                    agent=best_tf_agent)
-                summary_writer_manager = self.summary_writer_manager_factory.get_summary_writer_manager(
-                    tf_agent=best_tf_agent)
+                self.best = self.agent_copier.copy_agent(
+                    meta_agent=meta_agent, name="best")
 
-                best_meta_agent = MetaAgent(
-                    tf_agent=best_tf_agent, checkpoint_manager=agent_checkpoint_manager, summary_writer_manager=summary_writer_manager)
-                best_meta_agent.fitness = meta_agent.fitness
-                best_meta_agent.previous_fitness = meta_agent.previous_fitness
-                best_meta_agent.tweak_probability = meta_agent.tweak_probability
-                self.best = best_meta_agent
+                if self.best_possible_fitness and self.best.fitness >= self.best_possible_fitness:
+                    print("best_fitness: ", self.best.fitness)
+                    return self.best
+
+        print("best_fitness: ", self.best.fitness)
 
         return self.best
-
-    def train_with_gradient_based_approach(self, meta_agent: MetaAgent):
-        collect_policy = py_tf_eager_policy.PyTFEagerPolicy(
-            meta_agent.tf_agent.collect_policy, use_tf_function=True)
-        # ToDo: change num_steps
-        collect_driver = self.collect_driver_factory.get_driver(
-            policy=collect_policy, num_steps=1)
-
-        self.gradient_based_trainer.train_agent(
-            meta_agent=meta_agent, collect_driver=collect_driver)
