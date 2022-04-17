887f58e6da510891d37315c4dd0aaaf3f85fa1ef
Falguni Das <dasshuvo7@gmail.com>
2022-04-17 17:13:03 +0000

    Add agent crossover

--- src/agent/meta_agent/meta_agent_copier.py
+++ src/agent/meta_agent/meta_agent_copier.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from fitness_evaluator.fitness_evaluator import FitnessEvaluator
 
 from agent.meta_agent.meta_agent import MetaAgent
 
@@ -7,3 +8,8 @@ class MetaAgentCopier(ABC):
     @abstractmethod
     def copy_agent(self, meta_agent: MetaAgent, name: str):
         pass
+
+    @abstractmethod
+    def crossover(self, agent_1: MetaAgent, agent_2: MetaAgent,
+                  agent_1_keep_precentage: float, fitness_evaluator: FitnessEvaluator):
+                  pass
