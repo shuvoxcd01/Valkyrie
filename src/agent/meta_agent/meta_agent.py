524d4d1ef6f84348ed40f5bfb48e81ea1abd56ee
Falguni Das <dasshuvo7@gmail.com>
2022-05-01 13:40:18 +0000

    Deprecate parameters.

--- src/agent/meta_agent/meta_agent.py
+++ src/agent/meta_agent/meta_agent.py
@@ -8,15 +8,13 @@ from checkpoint_manager.agent_checkpoint_manager import AgentCheckpointManager
 class MetaAgent(ABC):
     def __init__(self, tf_agent: TFAgent, checkpoint_manager: AgentCheckpointManager,
                  summary_writer_manager: SummaryWriterManager,
-                 fitness=0, previous_fitness=0, tweak_probability=0, beta=0,
+                 fitness=0, previous_fitness=0,
                  generation: int = 0, name=None) -> None:
         self.tf_agent = tf_agent
         self.checkpoint_manager = checkpoint_manager
         self.summary_writer_manager = summary_writer_manager
         self.previous_fitness = previous_fitness
         self.fitness = fitness
-        self.tweak_probability = tweak_probability
-        self.beta = beta
         self.generation = generation
         self.name = self.tf_agent.name if name is None else name
 
