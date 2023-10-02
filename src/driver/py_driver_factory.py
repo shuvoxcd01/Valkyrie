523615219ccfb1e9af6dbc87ff3b2a873911fdc3
shuvoxcd01 <dasshuvo7@gmail.com>
2023-10-02 23:02:37 +0000

    Add logging

--- src/driver/py_driver_factory.py
+++ src/driver/py_driver_factory.py
@@ -3,11 +3,13 @@ from driver.driver_factory import DriverFactory
 from tf_agents.environments import PyEnvironment
 from tf_agents.drivers import py_driver
 from tf_agents.policies import py_policy
+import logging
 
 
 class PyDriverFactory(DriverFactory):
     def __init__(self, common_observers: Optional[List]) -> None:
         super().__init__(common_observers=common_observers)
+        self.logger = logging.getLogger(__file__)
 
     def _get_driver(
         self,
@@ -15,9 +17,18 @@ class PyDriverFactory(DriverFactory):
         observers: List,
         policy: py_policy.PyPolicy,
         max_steps: int,
-        **kwargs
+        **kwargs,
     ):
         max_episodes = kwargs.get("max_episodes", 1)
+
+        self.logger.info(
+            f"""Creating dynamic step driver with the following params: 
+            env:{env}, 
+            observers: {observers}, 
+            policy: {policy}, 
+            max_steps: {max_steps},
+            max_episodes: {max_episodes}"""
+        )
 
         return py_driver.PyDriver(
             env=env,
