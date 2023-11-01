64359f9d4c6f7e98a230461c510b9991fa4bcabd
shuvoxcd01 <dasshuvo7@gmail.com>
2023-11-01 16:02:26 +0000

    Simplify agent specific network

--- src/network/agent_network/q_networks/atari/atari_q_network.py
+++ src/network/agent_network/q_networks/atari/atari_q_network.py
@@ -1,6 +1,7 @@
-from typing import List
+from typing import List, Optional, Sequence, Text
 from tf_agents.networks import q_network
 import tensorflow as tf
+from tf_agents.networks import sequential
 
 import logging
 
@@ -10,78 +11,35 @@ from Valkyrie.src.network.pretraining_network.atari.atari_pretraining_network im
 )
 
 
-class AtariQNetwork(q_network.QNetwork, BaseQNetwork):
+class AtariQNetwork(BaseQNetwork, sequential.Sequential):
     def __init__(
         self,
         pretraining_network,
-        input_tensor_spec,
-        action_spec,
-        preprocessing_layers=None,
-        preprocessing_combiner=None,
-        conv_layer_params=None,
-        fc_layer_params=...,
-        dropout_layer_params=None,
-        activation_fn=tf.keras.activations.relu,
-        kernel_initializer=None,
-        batch_squash=True,
-        dtype=tf.float32,
-        name="QNetwork",
+        layers: Sequence[tf.keras.layers.Layer],
+        input_spec=None,
+        name: Optional[Text] = None,
+        **kwargs,
     ):
+        BaseQNetwork.__init__(self)
+        sequential.Sequential.__init__(self, layers, input_spec, name)
+
         self.pretraining_network = pretraining_network
 
-        if kernel_initializer is None:
-            logging.info("No explicit initializer provided for AtariQNetwork.")
-        else:
-            logging.info(
-                f"Explicitly provided AtariQNetwork initializer: {kernel_initializer}"
-            )
+    def call(self, inputs, network_state=..., **kwargs):
+        inputs = tf.cast(inputs, tf.float32)
+        inputs = inputs / 255
 
-        q_network.QNetwork.__init__(
-            self,
-            input_tensor_spec,
-            action_spec,
-            preprocessing_layers,
-            preprocessing_combiner,
-            conv_layer_params,
-            fc_layer_params,
-            dropout_layer_params,
-            activation_fn,
-            kernel_initializer,
-            batch_squash,
-            dtype,
-            name,
-        )
-        BaseQNetwork.__init__(self)
-
-    def call(self, observation, step_type=None, network_state=(), training=False):
         # _shape = observation.shape
-        # assert len(_shape) == 5, "Input observation is expected to have 5 dims."
+        # if _shape == tf.TensorShape([1, 21, 21, 8]):
+        #     features = observation
+        # else:
+        features = self.pretraining_network(inputs)
+        output = super().call(features, network_state, **kwargs)
 
-        # batch, step = _shape[0], _shape[1]
+        return output
 
-        # observation = tf.reshape(
-        #     tensor=observation, shape=[batch * step, _shape[2], _shape[3], _shape[4]]
-        # )
-        observation = tf.cast(observation, tf.float32)
-        observation = observation / 255
+    def get_mutation_layers(self):
+        return self.layers
 
-        _shape = observation.shape
-        if _shape == tf.TensorShape([1, 21, 21, 8]):
-            features = observation
-        else:
-            features = self.pretraining_network(observation)
-
-        return super(AtariQNetwork, self).call(
-            features,
-            step_type=step_type,
-            network_state=network_state,
-            training=training,
-        )
-
-    def get_mutation_layers(self) -> List:
-        embedding_network = self.layers[0]
-        return [embedding_network.layers[-1]]
-
-    def get_crossover_layers(self) -> List:
-        embedding_network = self.layers[0]
-        return [embedding_network.layers[-1]]
+    def get_crossover_layers(self):
+        return self.layers
--- src/network/agent_network/q_networks/atari/atari_q_network_factory.py
+++ src/network/agent_network/q_networks/atari/atari_q_network_factory.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from Valkyrie.src.network.agent_network.network_factory import NetworkFactory
 from Valkyrie.src.network.agent_network.q_networks.atari.atari_q_network import (
     AtariQNetwork,
@@ -5,6 +6,7 @@ from Valkyrie.src.network.agent_network.q_networks.atari.atari_q_network import 
 from Valkyrie.src.network.pretraining_network.atari.atari_pretraining_network import (
     AtariPretrainingNetwork,
 )
+from tf_agents.specs import tensor_spec
 
 
 class AtariQNetworkFactory(NetworkFactory):
@@ -18,20 +20,42 @@ class AtariQNetworkFactory(NetworkFactory):
     ) -> None:
         super().__init__()
 
+        self.action_tensor_spec = tensor_spec.from_spec(action_spec)
+        self.num_actions = (
+            self.action_tensor_spec.maximum - self.action_tensor_spec.minimum + 1
+        )
         self.pretraining_network_encoder = pretraining_network.get_pretraining_network()
         self.input_tensor_spec = input_tensor_spec
         self.action_spec = action_spec
         self.conv_layer_params = conv_layer_params
         self.fc_layer_params = fc_layer_params
 
+    def _create_dense_layer(self, num_units):
+        return tf.keras.layers.Dense(
+            num_units,
+            activation=tf.keras.activations.relu,
+            kernel_initializer=tf.keras.initializers.VarianceScaling(
+                scale=2.0, mode="fan_in", distribution="truncated_normal"
+            ),
+        )
+
     def get_network(self, kernel_initializer=None):
+        q_values_layer = tf.keras.layers.Dense(
+            self.num_actions,
+            activation=None,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=tf.keras.initializers.Constant(-0.2),
+        )
+
+        dense_layers = [
+            self._create_dense_layer(num_units) for num_units in self.fc_layer_params
+        ]
+
+        trainable_layers = dense_layers + [q_values_layer]
+
         q_net = AtariQNetwork(
             pretraining_network=self.pretraining_network_encoder,
-            input_tensor_spec=self.input_tensor_spec,
-            action_spec=self.action_spec,
-            conv_layer_params=self.conv_layer_params,
-            fc_layer_params=self.fc_layer_params,
-            kernel_initializer=kernel_initializer,
+            layers=trainable_layers,
         )
 
         return q_net
